1362bd7bc102ebf005697b71ea09c1a87f534f93
eander0105 <eander0105@gmail.com>
2021-12-14 15:23:01 +0100

    basic login functionality

---
+++ server/db.js
@@ -0,0 +1,36 @@
+var db = require('mysql')
+
+// Connects to sql server
+const connection = db.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root'
+});
+connection.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MYSQL connected');
+});
+
+// Create database if not already extit
+sql = 'CREATE DATABASE IF NOT EXISTS ReactWidgetApp';
+connection.query(sql, (err, results) => {
+    if(err){
+        throw err;
+    }
+    console.log('DB created');
+});
+
+// Selects database
+connection.query('USE ReactWidgetApp')
+
+// Creats tables if it does not already exist
+sql = 'CREATE TABLE IF NOT EXISTS users(id int AUTO_INCREMENT, firstname VARCHAR(255), lastname VARCHAR(255), username VARCHAR(255), password VARCHAR(255), PRIMARY KEY(id))';
+connection.query(sql, (err, results) => {
+    if(err) throw err;
+    console.log('Table Created');
+});
+
+
+module.exports = connection;
