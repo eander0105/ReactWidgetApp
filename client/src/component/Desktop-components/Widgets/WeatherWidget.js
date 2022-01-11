aa07242017c2f4bb390a227cd93001ada6d13ae7
eander0105 <eander0105@gmail.com>
2022-01-11 15:13:48 +0100

    API call weatherbit, draggable

---
+++ client/src/component/Desktop-components/Widgets/WeatherWidget.js
@@ -0,0 +1,84 @@
+import React, {useState, useEffect} from 'react'
+import './Widgets.css'
+import axios from 'axios';
+import { useTimer } from 'react-timer-hook';
+import Cookies from 'js-cookie';
+
+function WeatherWidget(props) {
+
+    const [weatherData, setWeatherData] = useState(false);
+    const [forcast, setForcast] = useState(false)
+
+    // https://api.weatherbit.io/v2.0/current?lat=58.4094&lon=15.6257&lang=sv&key=1b76a232bb664b5d904fc81b0fbc1d25 , {lat: 58.4094, lon: 15.6257, lang: 'sv', key: '1b76a232bb664b5d904fc81b0fbc1d25'}
+    
+    const APIkey = '1b76a232bb664b5d904fc81b0fbc1d25';
+
+    const userData = {
+        lat: '58.4094',
+        lon: '15.6257',
+    }
+
+    const fetchWeatherData = () => {
+
+        // Current weather API call
+        axios({
+            method: 'post',
+            url: 'https://api.weatherbit.io/v2.0/current',
+            params: {
+                key: APIkey,
+                lat: userData.lat,
+                lon: userData.lon,
+                lang: 'sv'
+            }
+        }).then((res) => {
+                setWeatherData(res);
+                
+                // Start Timer to fetch data in 10 min
+                const newTime = new Date()
+                newTime.setSeconds(newTime.getSeconds() + 600) // Timer 10 min from now
+                restart(newTime);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+
+        //Coming week forcast API call
+        axios({
+            method: 'post',
+            url: 'https://api.weatherbit.io/v2.0/forecast/daily',
+            params: {
+                key: APIkey,
+                lat: userData.lat,
+                lon: userData.lon,
+                lang: 'sv'
+            }
+        }).then((res) => {
+                setForcast(res);
+                
+                // Start Timer to fetch data in 10 min
+                const newTime = new Date()
+                newTime.setSeconds(newTime.getSeconds() + 600) // Timer 10 min from now
+                restart(newTime);
+            }).catch((err) => {
+                console.error(err);
+            });
+
+    }
+
+    // Timer
+    const {
+          restart
+        } = useTimer({ onExpire: () => fetchWeatherData() });
+    
+    useEffect(() => {
+        fetchWeatherData();
+    }, [])
+    
+    return (
+        <div className='Widget Weather'>
+            {weatherData ? weatherData.data.data[0].temp : ''}
+        </div>
+    )
+}
+
+export default WeatherWidget
